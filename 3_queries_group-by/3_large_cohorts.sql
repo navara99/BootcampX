9066e140ea3770bf76f9a58a8bdf0592ab59a619
Tharsikan <thar.navaratnam@gmail.com>
2021-11-22 19:01:53 +0000

    add query for large cohorts

---
+++ 3_queries_group-by/3_large_cohorts.sql
@@ -0,0 +1,8 @@
+SELECT cohorts.name AS cohort_name , COUNT(students.id) AS student_count
+FROM students JOIN cohorts ON students.cohort_id = cohorts.id
+GROUP BY cohorts.name
+HAVING COUNT(students.id) >= 18
+ORDER BY student_count;
+
+
+
