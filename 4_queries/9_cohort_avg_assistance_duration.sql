2533436ad9a21d9af59b6d94fb2ec14d254bbdd0
Tharsikan <thar.navaratnam@gmail.com>
2021-11-23 20:55:36 +0000

    query for avg total assistance duration

---
+++ 4_queries/9_cohort_avg_assistance_duration.sql
@@ -0,0 +1,9 @@
+SELECT AVG(total_duration) AS average_total_duration
+FROM (
+  SELECT cohorts.name , SUM(assistance_requests.completed_at - assistance_requests.started_at) AS total_duration
+  FROM cohorts
+    JOIN students ON cohorts.id = students.cohort_id
+    JOIN assistance_requests ON assistance_requests.student_id = students.id
+  GROUP BY cohorts.name
+  ORDER BY total_duration
+) AS total_duration;
