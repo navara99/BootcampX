bd57c458abfa2e39e6323b95463424ece34c217c
Tharsikan <thar.navaratnam@gmail.com>
2021-11-23 21:55:37 +0000

    remove join on assignments table

--- 4_queries/12_teachers_that_assisted.sql
+++ 4_queries/12_teachers_that_assisted.sql
@@ -1,7 +1,6 @@
 SELECT DISTINCT teachers.name AS teacher , cohorts.name AS cohort
 FROM assistance_requests
   JOIN teachers ON teacher_id = teachers.id
-  JOIN assignments ON assignment_id = assignments.id
   JOIN students ON student_id = students.id
   JOIN cohorts ON students.cohort_id = cohorts.id
 WHERE cohorts.name = 'JUL02'
